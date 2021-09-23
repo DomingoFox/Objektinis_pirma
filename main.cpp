7ace61ffc03462843299066ae326f9349c304b24
DomingoFox <64082979+DomingoFox@users.noreply.github.com>
2021-09-23 18:43:10 +0300

    First version

---
+++ main.cpp
@@ -0,0 +1,90 @@
+
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+struct info
+{
+    string vardas, pavarde;
+    double nd_rezultatai[101], egz_rezultatas, galutinis;
+};
+
+void Skaityk(info studentas[101], int &n, int &namu_darbu_sk)
+{
+    cout << "Iveskite studentu skaiciu: " <<endl;
+    cin >> n;
+
+    cout << "Kiek namu darbu ivertinimu turejo studentai?" << endl;
+    cin >> namu_darbu_sk;
+    for(int i=0; i<n ; i++)
+    {
+        cout << "Iveskite " << i+1 << "-ojo studento varda ir pavarde" << endl;
+        cin >> studentas[i].vardas >> studentas[i].pavarde;
+
+        cout << "Iveskite studento ivertinimus: " <<endl;
+        for(int j=0; j<namu_darbu_sk; j++)
+        {
+            cin >> studentas[i].nd_rezultatai[j];
+        }
+
+        cout << "Iveskite " << i+1 << "-ojo studento egzamino rezultata" << endl;
+        cin >> studentas[i].egz_rezultatas;
+    }
+
+    return;
+}
+
+void Galutinis_balas(info studentas[101], int &n, int &namu_darbu_sk)
+{
+    for(int i=0; i<n; i++)
+    {
+        double nd_suma = 0;
+        for(int j=0; j<namu_darbu_sk; j++)
+        {
+            nd_suma += studentas[i].nd_rezultatai[j];
+        }
+        double vidurkis = nd_suma/namu_darbu_sk;
+        studentas[i].galutinis = 0.4 * vidurkis + 0.6 * studentas[i].egz_rezultatas;
+    }
+
+    return;
+}
+
+/*void Mediana(info studentas[101], int &n, int &namu_darbu_sk)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<namu_darbu_sk; j++)
+        {
+
+
+        }
+    }
+}*/
+
+void Rasyk(info studentas[101], int &n)
+{
+    cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis (Vid.)" <<endl;
+    cout << "-------------------------------------------------------------" <<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout << fixed << left << setw(20) << studentas[i].pavarde << setw(20) << studentas[i].vardas << setw(20) << setprecision(2) << studentas[i].galutinis <<endl;;
+    }
+
+    return;
+}
+
+
+int main()
+{
+    int n, namu_darbu_sk;
+    info studentas[101];
+
+    Skaityk(studentas,n,namu_darbu_sk);
+    Galutinis_balas(studentas,n,namu_darbu_sk);
+    Rasyk(studentas,n);
+    cout<<"hello"<<endl;
+
+    return 0;
+}
